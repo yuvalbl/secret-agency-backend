10f0d1b21e35d6b3b460bf69a5f4f803f0158a01
yuvalbl <yuval.bar.levi@gmail.com>
2018-06-28 10:14:45 +0300

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const db = require('./db');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/home', (req, res) => res.send('Home page'));
+
 app.get('/about', (req, res) => res.send('About page'));
+
+app.get('/posts/', (req, res) => {
+  res.send(JSON.stringify(db))
+});
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
