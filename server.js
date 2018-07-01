41f7fc2dccb46348aba95091c3efa4f8b9404138
yuvalbl <yuval.bar.levi@gmail.com>
2018-07-01 14:07:43 +0300

    basic api

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const db = require('./db');
 const express = require('express');
 const app = express();
 
-
 app.get('/about', (req, res) => res.send('About page'));
+
+app.get('/', (req, res) => {
+  res.redirect('/posts');
+});
 
 app.get('/posts/', (req, res) => {
   res.send(JSON.stringify(db))
 });
 
+app.get('/posts/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = db.find(post => post.id === id);
+  res.send(JSON.stringify(post));
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000,
+  () => console.log('Post API server listening on port 3000!')
+);
